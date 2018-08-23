8c87111b03b469077c8aa6069ea89ceafc2ba84e
mkatrichenko <35609113+mkatrichenko@users.noreply.github.com>
2018-08-23 21:20:23 +0300

    Speakers update

---
+++ js/lang/speakers-en.js
@@ -0,0 +1,67 @@
+export const SpeakersEn = [
+  {
+    image : '../public/img/speakers/vitaliy_kucheryaviy.jpg',
+    name : 'Vitaliy Kucheryaviy',
+    position : 'Fullstack Developer',
+    company : 'Superfast development with Django Prototyper',
+    rept : [
+      {
+        title : '',
+        description : 'Are you developing with Django ? Imagine creating your apps 10x ..100x times faster!\n' +
+        'I will show audience a live demo of the tool (django-prototyper) that automizes Django projects bootstrap and maitanance. \n' +
+        'That\'s a rich UI tool that allows generate complex db schemes, setups, plugable apps and configurations.'
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.linkedin.com/in/vitaliykucheraviy/',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://github.com/vitalik',
+        fatype : 'github'
+      },
+    ]
+  },
+  {
+    image : '../public/img/speakers/igor_davydenko.jpg',
+    name : 'Igor Davydenko',
+    position : 'Python/JS Developer',
+    company : '',
+    rept : [
+      {
+        title : '',
+        description : ''
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.facebook.com/playpauseandstop',
+        fatype : 'facebook'
+      },
+      {
+        link : 'https://github.com/playpauseandstop',
+        fatype : 'github'
+      },
+    ]
+  },
+ {
+    image : '../public/img/speakers/yehor_nazarkin.jpg',
+    name : 'Yehor Nazarkin',
+    position : 'Software Engineer',
+    company : 'DataRobot',
+    rept : [
+      {
+        title : '',
+        description : ''
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : []
+  },
+];
