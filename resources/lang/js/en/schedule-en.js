daaeea0db1b516bcb7e81291eebe22636d814a13
mkatrichenko <m_katrichenko@ukr.net>
2018-09-25 00:53:55 +0300

    shedule update and beetroot sponsor update

--- resources/lang/js/en/schedule-en.js
+++ resources/lang/js/en/schedule-en.js
@@ -1,30 +1,22 @@
 export const ScheduleEn = [
   {
-    time:'9.30-10.30',
+    time:'9.00-10.00',
     icon:'profile',
     scheduleRendered: '',
     talks: [
-      {author: '', title: 'Registration'},
+      {author: '', title: 'Registrartion'},
     ]
   },
   {
-    time:'10.30-10.40',
-    icon:'people',
+    time:'10.00-10.40',
+    icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: 'Conference opening'},
+      {author: 'Yehor Nazarkin', title: 'Journey to distributed task queue'},
     ]
   },
   {
-    time:'10.40-11.20',
-    icon:'people',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Conference start  <br> <br>  1st report'},
-    ]
-  },
-  {
-    time:'11.20-11.40',
+    time:'10.40-10.50',
     icon:'clock',
     scheduleRendered: '',
     talks: [
@@ -32,15 +24,15 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'11.40-12.20',
+    time:'10.50-11.30',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '2nd report'},
+      {author: 'Vsevolod Solovyov', title: 'How to debug. Everything.'},
     ]
   },
   {
-    time:'12.20-12.40',
+    time:'11.30-11.45',
     icon:'cup',
     scheduleRendered: '',
     talks: [
@@ -48,15 +40,15 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'12.40-13.20',
+    time:'11.45-12.25',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '3rd report'},
+      {author: 'Anton Caceres', title: 'Async Python and Web Sockets'},
     ]
   },
   {
-    time:'13.30-15.00',
+    time:'12.25-13.30',
     icon:'plate',
     scheduleRendered: '',
     talks: [
@@ -64,15 +56,15 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'15.00-15.40',
+    time:'13.30-14.10',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '4th report'},
+      {author: 'Andrii Soldatenko', title: 'Competitive programming using Python'},
     ]
   },
   {
-    time:'15.40-16.00',
+    time:'14.10-14.20',
     icon:'clock',
     scheduleRendered: '',
     talks: [
@@ -80,15 +72,15 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'16.00-16.40',
+    time:'14.20-15.00',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '5th report'},
+      {author: 'Igor Davydenko', title: 'How I migrate from REST API to GraphQL'},
     ]
   },
   {
-    time:'16.40-17.10',
+    time:'15.00-15.15',
     icon:'cup',
     scheduleRendered: '',
     talks: [
@@ -96,15 +88,15 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'17.10-17.50',
+    time:'15.15-15.55',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '6th report'},
+      {author: 'Dmitry Kondratenko', title: ''},
     ]
   },
   {
-    time:'17.50-18.10',
+    time:'15.55-16.05',
     icon:'clock',
     scheduleRendered: '',
     talks: [
@@ -112,15 +104,64 @@ export const ScheduleEn = [
     ]
   },
   {
-    time:'18.10-18.50',
+    time:'16.05-16.45',
     icon:'sheet',
     scheduleRendered: '',
     talks: [
-      {author: '', title: '7th report'},
+      {author: 'Alexander Solovyov', title: 'Make it fast'},
     ]
   },
   {
-    time:'19.00',
+    time:'16.45-17.00',
+    icon:'clock',
+    scheduleRendered: '',
+    talks: [
+      {author: '', title: 'Break'},
+    ]
+  },
+  {
+    time:'17.00-17.40',
+    icon:'sheet',
+    scheduleRendered: '',
+    talks: [
+      {author: 'Vitaliy Kucheryaviy', title: 'Django-prototyper - from idea to deploy in 5 minuets'},
+      {author: 'Alexey Milogradov', title: 'Django ORM: General Overview'},
+    ]
+  },
+  {
+    time:'17.40-17.50',
+    icon:'clock',
+    scheduleRendered: '',
+    talks: [
+      {author: '', title: 'Break'},
+    ]
+  },
+  {
+    time:'17.50-18.30',
+    icon:'sheet',
+    scheduleRendered: '',
+    talks: [
+      {author: 'Vitaliy Androsenko', title: 'SQLAachemy: practic magic'},
+    ]
+  },
+  {
+    time:'18.30-18.40',
+    icon:'clock',
+    scheduleRendered: '',
+    talks: [
+      {author: '', title: 'Break'},
+    ]
+  },
+  {
+    time:'18.40-19.20',
+    icon:'sheet',
+    scheduleRendered: '',
+    talks: [
+      {author: 'Serhii Ruskykh', title: 'Can be code quality measured via "testability"?'},
+    ]
+  },
+  {
+    time:'19.30',
     icon:'people',
     scheduleRendered: '',
     talks: [
